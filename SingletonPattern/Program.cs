3cc82e787f9f8bc5802bfb0e9b0cdaa273610a22
Ossama Edbali <o.edbali@ucas.ac.uk>
2019-03-03 21:21:57 +0000

    Add singleton pattern

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singletonInstance1 = Singleton.GetInstance();
+            var singletonInstance2 = Singleton.GetInstance();
+
+            Console.WriteLine(singletonInstance1 == singletonInstance2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,11 @@
+﻿namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private Singleton() { }
+
+        public static Singleton GetInstance() => _instance ?? (_instance = new Singleton());
+    }
+}
